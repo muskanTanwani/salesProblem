1aa56560639135511a3a9b80eff33b67bf3e96c3
muskanTanwani <125471773+muskanTanwani@users.noreply.github.com>
2023-02-25 01:26:14 +0530

    Add files via upload

---
+++ inp.rb
@@ -0,0 +1,22 @@
+class User 
+  def initialize
+    @arr = []
+  end
+ 
+  def takinginput
+    puts "Total number of Purchases : "
+    n = gets
+    n = n.chomp
+    n = n.to_i
+    puts "Please enter your order list one by one : "
+    for value in 1..n
+      order = gets
+      order = order.chomp
+      @arr.push(order)
+    end
+  end
+  def get
+    @arr
+  end
+end
+
